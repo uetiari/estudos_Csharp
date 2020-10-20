5c350df0f68bc5e873a5835281d2989dad84eb15
Ariane Ueti <42356440+uetiari@users.noreply.github.com>
2020-10-20 11:28:47 -0300

    Atribuição de Classe

---
+++ atribuicao.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace UdemyCourse {
+    class Program {
+        public static void Main(string[] args) {
+            
+            int a = 10;
+            a++; //acrescenta 1 a variável
+            Console.WriteLine(a);
+            
+            a--; //diminui 1 a variavel
+            
+            //int a = 10;
+            //int b = a++; 
+            //primeiro o valor de A, depois incrementa valor do A. Sendo B = 10 e A =11
+            
+            //int a = 10;
+            //int b = ++a;
+            //primeiro incrementa A e depois passa valor para B. Sendo A e B = 11
+            
+            
+            
+        }
+    }
+}
